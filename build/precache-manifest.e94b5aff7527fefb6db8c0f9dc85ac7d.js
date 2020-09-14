e82e71a4826407b5c0e5d3a35c95256175af9f7b
netn10 <34625504+netn10@users.noreply.github.com>
2020-09-14 14:56:12 +0300

    Add files via upload

---
+++ build/precache-manifest.e94b5aff7527fefb6db8c0f9dc85ac7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14d39065d41911037f8ceeebbcf76569",
+    "url": "/Speed-Typing-Game/index.html"
+  },
+  {
+    "revision": "bef81a180dc2f99d906a",
+    "url": "/Speed-Typing-Game/static/css/main.26a7409f.chunk.css"
+  },
+  {
+    "revision": "203259f01c77ee1f7ffc",
+    "url": "/Speed-Typing-Game/static/js/2.ef897a34.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Speed-Typing-Game/static/js/2.ef897a34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bef81a180dc2f99d906a",
+    "url": "/Speed-Typing-Game/static/js/main.a8a9e09a.chunk.js"
+  },
+  {
+    "revision": "3fd5f1107dab54f1355b",
+    "url": "/Speed-Typing-Game/static/js/runtime-main.124965d8.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import './App.css';
+import Header from './components/Header'
+import Body from './components/Body'
+import GameBoard from './components/GameBoard'
+
+import './styles.scss';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <Header />
+        <Body />
+        <GameBoard />
+      </div>
+    </>
+  );
+}
+
+export default App;
