5846b726f43c8838b74172244e687bf528f6c4ad
sahar227 <sahar22798@gmail.com>
2020-11-03 17:16:16 +0200

    Updates

--- precache-manifest.164a89978c831310d23f2a535a1ff0b9.js
+++ precache-manifest.164a89978c831310d23f2a535a1ff0b9.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c850457dacc34284504f7722001bbe50",
+    "revision": "6ec1747fbdac4822bfb0d910dcc82e09",
     "url": "./index.html"
   },
   {
-    "revision": "a262b1edac6c50e37e26",
+    "revision": "b187d9e693f97208204a",
     "url": "./static/css/main.c55c21c9.chunk.css"
   },
   {
-    "revision": "b85095780a30ffa33d61",
-    "url": "./static/js/2.321f1613.chunk.js"
+    "revision": "6a3e6d7e4478456dcb09",
+    "url": "./static/js/2.c73668e6.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "./static/js/2.321f1613.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.c73668e6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a262b1edac6c50e37e26",
-    "url": "./static/js/main.f9cc2fac.chunk.js"
+    "revision": "b187d9e693f97208204a",
+    "url": "./static/js/main.8275e33f.chunk.js"
   },
   {
     "revision": "bb406576fc2b3ccc4050",
