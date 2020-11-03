1465e1075cdfa553068dc6df46a887c263cefb6e
sahar227 <sahar22798@gmail.com>
2020-11-03 17:07:55 +0200

    Updates

---
+++ precache-manifest.c13404e28fb15431b392e5ce5d1f77cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db7d6527ba6f2fe9881bfbe45965bc21",
+    "url": "./index.html"
+  },
+  {
+    "revision": "94156ced1833672525fa",
+    "url": "./static/css/main.c55c21c9.chunk.css"
+  },
+  {
+    "revision": "b85095780a30ffa33d61",
+    "url": "./static/js/2.321f1613.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.321f1613.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94156ced1833672525fa",
+    "url": "./static/js/main.a183143e.chunk.js"
+  },
+  {
+    "revision": "bb406576fc2b3ccc4050",
+    "url": "./static/js/runtime-main.d5852e38.js"
+  }
+]);
